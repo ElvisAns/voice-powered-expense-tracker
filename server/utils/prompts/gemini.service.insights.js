fffab7448eccfd004b2b47ed6e481f90baa6d477
ANSIMA Elvis <ansimapersic@gmail.com>
2024-08-11 22:56:09 +0200

    slightly improve the insight prompt to avoid gemini being too technical eg. answering with json within the response body

--- server/utils/prompts/gemini.service.insights.js
+++ server/utils/prompts/gemini.service.insights.js
@@ -4,6 +4,7 @@ async function run(userinput) {
     const parts = [
         { text: `I will give you a informations about my spendings/earning in the last 7 days the you will have to extract some insights for me.
             The insight can be anything related to analytics (how i am spending, how to optimize my spending, what did i spent the most on,etc...).
+            If no enough information provide, just encourage me to keep tracking my expense so i can get your insights.
             `
         }, {
             text : `Here is the information in json format for you to easy understand : ${userinput}`
